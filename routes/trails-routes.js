3c65c351cc4c39212f799853c412638112978cdc
Genet worku <genetmworku@gmail.com>
2020-05-05 13:53:19 -0600

    update

--- routes/trails-routes.js
+++ routes/trails-routes.js
@@ -5,14 +5,14 @@ var db = require("../models");
 module.exports = function(app) {
 
     app.get("/api/trails", function(req, res) {
-        db.Trails.findAll({}).then(function(dbTrail) {
+        db.Trail.findAll({}).then(function(dbTrail) {
           res.json(dbTrail);
         });
       });
     
 
   app.get("/api/trails/:longitude/:latitude", function(req, res) {
-    db.Trails.findOne({
+    db.Trail.findOne({
       where: {
        longitude:req.params.longitude,
        latitude :req.params.latitude
@@ -24,7 +24,7 @@ module.exports = function(app) {
   });
 
   app.post("/api/trails", function(req, res) {
-    db.Trails.create(req.body).then(function(dbTrail) {
+    db.Trail.create(req.body).then(function(dbTrail) {
       res.json(dbTrail);
     });
   });

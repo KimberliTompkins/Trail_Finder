b1767193c58425ec7fad0956900479394cc67c52
Genet worku <genetmworku@gmail.com>
2020-05-07 18:26:25 -0600

    wraped map and search in continers and added margens

--- routes/trails-routes.js
+++ routes/trails-routes.js
@@ -1,33 +1,8 @@
-
 var db = require("../models");
 const axios = require("axios");
 
 module.exports = app => {
 
-<<<<<<< HEAD
-    app.get("/api/trails", function(req, res) {
-        db.Trail.findAll({}).then(function(dbTrail) {
-          res.json(dbTrail);
-        });
-      });
-    
-
-  app.get("/api/trails/:longitude/:latitude", function(req, res) {
-    db.Trail.findOne({
-      where: {
-       longitude:req.params.longitude,
-       latitude :req.params.latitude
-      }
-    }).then(function(dbTrail) {
-      console.log(dbTrail);
-      res.json(dbTrail);
-    });
-  });
-
-  app.post("/api/trails", function(req, res) {
-    db.Trail.create(req.body).then(function(dbTrail) {
-      res.json(dbTrail);
-=======
 
   app.get("/api/trails/:longitude/:latitude", function (req, res) {
     axios.get(`https://www.hikingproject.com/data/get-trails?lat=${req.params.latitude}&lon=${req.params.longitude}&maxDistance=10&key=${process.env.TRAIL_PROJECT_KEY}`)
@@ -44,7 +19,6 @@ module.exports = app => {
   app.get("/api/trails/location", (req, res) => {
     db.Location.findAll({}).then(dbLocation => {
       res.json(dbLocation);
->>>>>>> master
     });
   });
   
@@ -60,4 +34,4 @@ module.exports = app => {
       });
   });
 
-};
+}

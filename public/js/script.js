9d56dd5fde392913a8d2be133a514bd52b68efc0
Kim Tompkins <kimberli.tompkins@outlook.com>
2020-05-15 20:07:11 -0600

    moved the forecast above the map.  removed the serch and forecast pages.  / path now goes to the index page.  login is in the nav

--- public/js/script.js
+++
@@ -1,5 +0,0 @@
-var favorite1 = $.get("/api/trails").then(function (data) {
-  console.log(data);
-});
-
-var num1 = $("<div>" + "Trail Name: " + "" + "</div>");
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,10 +7,9 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function (app) {
   app.get("/", function (req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    
+      res.redirect("/index");
+    
   });
 
   app.get("/login", function (req, res) {
